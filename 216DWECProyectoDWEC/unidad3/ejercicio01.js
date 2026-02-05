1fb591c58f5d41cda12d9f5fff33a2e1d150c177
IRENE RODRÍGUEZ RODRÍGUEZ <cuentadeire@gmail.com>
2026-02-05 15:16:02 +0100

    ✨DWEC unidad 3

---
+++ 216DWECProyectoDWEC/unidad3/ejercicio01.js
@@ -0,0 +1,17 @@
+/*
+ Número de actividad: 01
+ Enunciado: Crea un objeto en JavaScript. 
+            Escribe un objeto llamado persona con las propiedades nombre, edad y profesion. 
+            Muestra el nombre en consola.
+
+ Fecha de realización: 26 de octubre de 2025
+ Última revisión:  27 de octubre de 2025
+*/
+
+const persona = {
+    nombre : "Irene Rodríguez",
+    edad : 21,
+    profesion : "Diseñadora gráfica"
+};
+
+console.log(persona.nombre);
---
+++ 216DWECProyectoDWEC/unidad3/ejercicio02.js
@@ -0,0 +1,10 @@
+/*
+ Número de actividad: 02
+ Enunciado: Escribe un código que muestre en consola el ancho de la pantalla usando el objeto screen.
+
+ Fecha de realización: 26 de octubre de 2025
+ Última revisión:  27 de octubre de 2025
+*/
+
+var anchoPantalla = screen.width;
+console.log("La medida de ancho de la pantalla es " + anchoPantalla + " píxeles.");
